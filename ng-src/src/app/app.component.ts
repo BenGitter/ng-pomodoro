2566bcab99ad571c5f7618c7a2ec5304b0b8c1e4
Benjamin van Zwienen <benjaminvanzwienen@live.nl>
2018-01-08 23:50:49 +0100

    Add frontend logic to get authentication JWT

--- ng-src/src/app/app.component.ts
+++ ng-src/src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  popup: any;
+  token: string = '';
+
+  constructor(){
+    window.addEventListener("message", (e) => {
+      if(e.data.completed){
+        // Received correct message
+      
+        if(e.data.token){
+          // got token
+          this.token = e.data.token;
+          console.log(e.data.token);
+        }else{
+          // error
+          console.log('An error occured');
+        }
+
+        this.popup.close();
+      }
+    });  
+  }
+
+  onLoginTrello(){
+    console.log("LOGIN...");
+
+    this.popup = window.open("http://localhost:3000/auth/trello", "Login", "height=700,width=500,status=no,menubar=no,toolbar=no,personalbar=no,resizable=no");
+
+  }
 }
